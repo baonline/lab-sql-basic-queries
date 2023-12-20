992eef149533ba7e114fc276c91862e873f17f0d
Bryan A. Ortega León <109978327+baonline@users.noreply.github.com>
2023-12-20 11:03:40 -0400

    Solution after review

--- lab-sql-basic-queries-solved.sql
+++ lab-sql-basic-queries-solved.sql
@@ -4,24 +4,23 @@ tables;
 -- Retrieve all the data from the tables actor, film and customer.
 select * 
 from actor;
-
 select * 
 from film;
-
 select * 
 from customer;
         
 -- Retrieve the following columns from their respective tables:
 -- 3.1 Titles of all films from the film table
+-- 3.2 List of languages used in films, with the column aliased as language from the language table
+-- 3.3 List of first names of all employees from the staff table
 select 
 title 
-from film;
-
--- 3.2 List of languages used in films, with the column aliased as language from the language table
-select * 
-from language;
-
--- 3.3 List of first names of all employees from the staff table
+from film
+;
+select 
+name as language 
+from language
+;
 select 
 first_name 
 from staff
@@ -30,26 +29,30 @@ from staff
 -- Retrieve unique release years.
 select distinct 
 release_year 
-from film;
+from film
+;
 
 -- Counting records for database insights:
 -- 5.1 Determine the number of stores that the company has.
 select 
 count(store_id) as 'Number of stores'
 from store;
+-- select count(*) from store;
 
 -- 5.2 Determine the number of employees that the company has.
 select 
 count(staff_id) as 'number of employees' 
 from staff;
+-- select count(*) from staff;
 
 -- 5.3 Determine how many films are available for rent and how many have been rented.
-select *
+select * from rental;
+-- select count(rental_id) from rental where return_date is not null;
+-- select count(distict film_id) from inventory;
 
-from rental;
 -- 5.4 Determine the number of distinct last names of the actors in the database.
 select distinct
-last_name 
+count(last_name)
 from actor
 ;
 -- Retrieve the 10 longest films.
